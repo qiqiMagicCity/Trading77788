ae749e7f597a2c4b33da58567ae1b9be1844eb53
qiqiMagicCity <QiQi_MagicCity@outlook.com>
2025-07-04 15:36:37 -0400

    5.3.20

--- js/symbols.js
+++ js/symbols.js
@@ -664,6 +664,8 @@ window.SymbolCN = {
   "COPX": "铜矿ETF",
   "REMX": "稀土与战略金属ETF",
   "WOOD": "木材ETF",
+  "TSM": "台积电",
+  "SONY": "索尼",
   "PAVE": "基础设施ETF",
   "IFRA": "美国基础设施ETF",
   "IGF": "全球基础设施ETF",
