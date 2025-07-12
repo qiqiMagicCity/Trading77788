8283a549634541c24e5d35876c0d3a88a9cb0e2e
qiqiMagicCity <QiQi_MagicCity@outlook.com>
2025-07-12 11:37:08 -0400

    7.87

--- modules/M12/logic.js
+++ modules/M12/logic.js
@@ -1,31 +1,12 @@
 import ModuleBase from '../ModuleBase.js';
-import { getTrades, getDailyClose } from '../../utils/dataStore.js';
-import { getPrice } from '../../utils/priceService.js';
-
-function startOf(period, date=new Date()){
-  const d=new Date(date);
-  if(period==='week'){
-    const day=(d.getDay()+6)%7; // Monday start
-    d.setDate(d.getDate()-day);
-  }else if(period==='month'){
-    d.setDate(1);
-  }else if(period==='year'){
-    d.setMonth(0); d.setDate(1);
-  }
-  d.setHours(0,0,0,0);
-  return d;
-}
-
+import { getDailyResults, calcSince } from '../../utils/dailyResult.js';
 class M12Logic extends ModuleBase{
-  constructor(){ super('M12'); this.calc(); }
-  async calc(){
-    const trades = await getTrades();
-    const daily = await getDailyClose(); // assume array {date, float, realized}
-    const start = startOf('month');
-    let pnl=0;
-    daily.filter(r=>new Date(r.date)>=start).forEach(r=>{ pnl+=r.float + r.realized; });
-    this.publish({value:pnl});
-  }
+ constructor(){ super('M12'); this.calc(); }
+ async calc(){
+   const list = await getDailyResults();
+   const total = calcSince(list,'2025-07-01');
+   this.publish({value:total});
+ }
 }
 window['M12Logic']=new M12Logic();
 export default window['M12Logic'];
--- modules/M13/logic.js
+++ modules/M13/logic.js
@@ -1,31 +1,12 @@
 import ModuleBase from '../ModuleBase.js';
-import { getTrades, getDailyClose } from '../../utils/dataStore.js';
-import { getPrice } from '../../utils/priceService.js';
-
-function startOf(period, date=new Date()){
-  const d=new Date(date);
-  if(period==='week'){
-    const day=(d.getDay()+6)%7; // Monday start
-    d.setDate(d.getDate()-day);
-  }else if(period==='month'){
-    d.setDate(1);
-  }else if(period==='year'){
-    d.setMonth(0); d.setDate(1);
-  }
-  d.setHours(0,0,0,0);
-  return d;
-}
-
+import { getDailyResults, calcSince } from '../../utils/dailyResult.js';
 class M13Logic extends ModuleBase{
-  constructor(){ super('M13'); this.calc(); }
-  async calc(){
-    const trades = await getTrades();
-    const daily = await getDailyClose(); // assume array {date, float, realized}
-    const start = startOf('year');
-    let pnl=0;
-    daily.filter(r=>new Date(r.date)>=start).forEach(r=>{ pnl+=r.float + r.realized; });
-    this.publish({value:pnl});
-  }
+ constructor(){ super('M13'); this.calc(); }
+ async calc(){
+   const list = await getDailyResults();
+   const total = calcSince(list,'2025-01-01');
+   this.publish({value:total});
+ }
 }
 window['M13Logic']=new M13Logic();
 export default window['M13Logic'];
