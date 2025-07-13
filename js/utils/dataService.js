4b532c8182976222ca60afc77fbc83a19501ecd6
qiqiMagicCity <QiQi_MagicCity@outlook.com>
2025-07-13 15:37:29 -0400

    7.6

--- js/utils/dataService.js
+++
@@ -1,9 +0,0 @@
-// Auto-generated dataService.js
-export async function loadTrades(){
-    const res = await fetch('data/trades.json');
-    return res.ok ? await res.json() : [];
-}
-export async function loadCloses(){
-    const res = await fetch('data/close_prices.json');
-    return res.ok ? await res.json() : {};
-}
--- js/utils/dateUtils.js
+++
@@ -1,7 +0,0 @@
-
-export function nowNY(){
-  const local=new Date();
-  const utcMs=local.getTime()+local.getTimezoneOffset()*60000;
-  const offset=-4*3600000; // EDT
-  return new Date(utcMs+offset);
-}
